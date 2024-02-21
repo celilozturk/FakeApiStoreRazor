783be117e48caeab20f0003f65d656f1fc0965c8
Celil Ozturk <celil.ozturk@windowslive.com>
2024-02-21 12:08:34 +0300

    First commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using FakeApiStoreRazor.Pages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fakeapistorerazorpage.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+    public List<Product> Products { get; set; }
+    public async Task OnGet()
+    {
+        var httpClient= _httpClientFactory.CreateClient("BaseURL");
+        var response=await httpClient.GetFromJsonAsync<IEnumerable<Product>>("/products");
+        Products=response.ToList();
+       // await System.Console.Out.WriteLineAsync(response.ToString());
+    }
+}
---
+++ Pages/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace FakeApiStoreRazor.Pages.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+    }
+}
